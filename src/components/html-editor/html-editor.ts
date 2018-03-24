ff487607288b22fd1d27cb7ad0f59a875e2f47c3
rezabagusp <rezabaguspermana.rbp@gmail.com>
2018-03-24 23:44:55 +0700

    ckeditor plugin added

---
+++ src/components/html-editor/html-editor.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+/**
+ * Generated class for the HtmlEditorComponent component.
+ *
+ * See https://angular.io/api/core/Component for more info on Angular
+ * Components.
+ */
+@Component({
+  selector: 'html-editor',
+  templateUrl: 'html-editor.html'
+})
+export class HtmlEditorComponent {
+
+  @Input('content') public content: string;
+  @Output() public contentChanged: EventEmitter<string>;
+
+  constructor() {
+    console.log('HtmlEditor Component fired');
+    this.contentChanged = new EventEmitter<string>();    
+  }
+  onChange(e) {
+    this.contentChanged.emit(e);
+  }
+
+  onReady(e) {
+    this.contentChanged.emit(e);
+  }  
+  
+
+
+}
