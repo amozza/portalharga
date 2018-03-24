eebe0caf22dfc000db6c840fafce029b023da1f0
rezabagusp <rezabaguspermana.rbp@gmail.com>
2018-03-24 23:53:41 +0700

    add layout tambah berbagi file

--- src/pages/pengetahuan/berbagi-file-edit-tambah/berbagi-file-edit-tambah.ts
+++ src/pages/pengetahuan/berbagi-file-edit-tambah/berbagi-file-edit-tambah.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FileChooser } from '@ionic-native/file-chooser';
 
 /**
  * Generated class for the BerbagiFileEditTambahPage page.
@@ -15,11 +16,26 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class BerbagiFileEditTambahPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public fileChooser: FileChooser, public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad BerbagiFileEditTambahPage');
   }
-
+	chooseFile()
+  {
+      this.fileChooser.open()
+      .then(uri => 
+      {
+        console.log('file choosed')
+        alert('bisa coy')
+				// this.uriFile = uri;
+        // let temp = this.uriFile.split("/");
+        // let filename = temp[temp.length-1];
+        // this.fileName = filename.replace(/%20/g," ");
+        // let tempEx = filename.split(".");
+        // this.fileExtension = tempEx[tempEx.length-1];
+      })
+      .catch(e => alert(e));
+  }
 }
