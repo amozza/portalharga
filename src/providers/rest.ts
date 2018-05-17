98acfc24a1eb0ae8b3929296dca3c753597823fd
rezabagusp <rezabaguspermana.rbp@gmail.com>
2018-05-17 20:04:17 +0700

    penambahan fungsi download dengan file transfer

--- src/providers/rest.ts
+++ src/providers/rest.ts
@@ -6,6 +6,8 @@ import { Http, Headers, RequestOptions, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { JwtHelper, AuthHttp } from 'angular2-jwt';
 import { Transfer, FileUploadOptions, TransferObject } from '@ionic-native/transfer';
+import { File, FileEntry, IFile } from '@ionic-native/file';
+
 import 'rxjs/add/observable/throw';
 
 
@@ -15,6 +17,7 @@ export class RestProvider    {
 
   constructor(
       private http: Http,
+      private file: File,
       private toastCtrl: ToastController,
       private authHttp: AuthHttp,
       private loadingCtrl: LoadingController,
@@ -62,9 +65,35 @@ export class RestProvider    {
       .map( 
         (response: Response)=>{
           const body = response.json();
-          return body.data || {};          
+          console.log('respinya ', body)
+          return body || {};          
       })
   }
+  public delete(url: string, token:string, claims: any): Observable<{}> {
+    const header = new Headers();
+    console.log('TOKEN from post ' , token)
+    header.append('Content-type', 'application/json');
+    header.append('Authorization', 'Bearer '+ token);
+    return this.http.delete(url, {headers: header, body: claims})
+      .map( 
+        (response: Response)=>{
+          const body = response.json();
+          return body.data || {};          
+      })
+  }    
+
+  public patch(url: string, token:string, claims: any): Observable<{}> {
+    const header = new Headers();
+    console.log('TOKEN from patch ' , token)
+    header.append('Content-type', 'application/json');
+    header.append('Authorization', 'Bearer '+ token);
+    return this.http.patch(url, claims, {headers: header})
+      .map( 
+        (response: Response)=>{
+          const body = response.json();
+          return body.data || {};       
+      })
+  }    
 
   public upload = {
     fileUpload: (uri, imageUri, params)=>{
@@ -76,25 +105,23 @@ export class RestProvider    {
         loader.present();
         const fileTransfer: TransferObject = this.transfer.create();
 
-        let toke='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YWE2MWM0ZWZkN2M5NjcxMGQ1MWFlMzciLCJ1c2VyX2lkIjoxMTMsInVzZXJuYW1lIjoicGV0YW5pMjAxOCIsInRpbWUiOiJGcmksIDMwIE1hciAyMDE4IDIwOjIzOjQ5IEdNVCIsInJvbGUiOjQsImxvZ2luX3R5cGUiOjEsImlhdCI6MTUyMjQ0MTQyOX0.mrD9q-R3KibbiVLoKWhYFbzqYE-l3yFQKHsW9MZ1Yp8'
+        // let toke='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YWE2MWM0ZWZkN2M5NjcxMGQ1MWFlMzciLCJ1c2VyX2lkIjoxMTMsInVzZXJuYW1lIjoicGV0YW5pMjAxOCIsInRpbWUiOiJGcmksIDMwIE1hciAyMDE4IDIwOjIzOjQ5IEdNVCIsInJvbGUiOjQsImxvZ2luX3R5cGUiOjEsImlhdCI6MTUyMjQ0MTQyOX0.mrD9q-R3KibbiVLoKWhYFbzqYE-l3yFQKHsW9MZ1Yp8'
         var options: FileUploadOptions = {
           fileKey: 'file',
           fileName: params.fileName,
           chunkedMode: false,
           mimeType: params.mimeType,
           params: params,
-          headers: {'Authorization':'Bearer '+toke},
+          headers: {'Authorization':'Bearer '+params.token},
         }
 
         fileTransfer.upload(imageUri, uri, options)
           .then((res) => {
-          let data = JSON.parse(res.response);
-          alert(data)
-          console.log('balikan bukti file ', JSON.stringify(res.response))
+          let data = res.response;
           console.log(res+" Uploaded Successfully");
           loader.dismiss();
 
-          resolve(JSON.stringify(res.response))
+          resolve(data)
         },(err) => {
           loader.dismiss();
           alert(JSON.stringify(err))
@@ -102,9 +129,28 @@ export class RestProvider    {
         });
 
       })
-    } 
+    },
+    fileDownload: (uri, params) =>{
+      return new Promise((resolve, reject)=>{
+        let loader = this.loadingCtrl.create({
+          content: "Downloading..."
+        });
+        loader.present();
+        const fileTransfer: TransferObject = this.transfer.create();
+        fileTransfer.download(uri, this.file.externalDataDirectory + params.fileName, true)
+        .then( res =>{
+          loader.dismiss();;
+          console.log('berhaskil ganteng ', res.toURL())
+          resolve(res)
+        }, errr =>{
+          loader.dismiss();
+          alert(JSON.stringify(errr))
+        })
+      })
+    }
   }
 
+  //method for connection lost
   public showError(error:any){
     console.log('showerror fire')
     if(error.status === 0){ ///check if the connection fall
